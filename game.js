3d5e5976de587a5a57c62ffdad266b8728c532f6
weijinglin <weijinglin@sjtu.edu.cn>
2022-03-12 09:26:08 +0800

    3rd

--- game.js
+++ game.js
@@ -33,9 +33,9 @@ function game(map,b,man,size,count,x,y){
     let score = 0;
     let myDiv = document.getElementById('all');
     let legth = size*50;
-    let attri = length +'px';
-    myDiv.style.height = attri;
-    myDiv.style.width = attri;
+    let attri = legth +'px';
+    myDiv.style.height = '0px';
+    myDiv.style.width = '0px';
     var count = 0;
     // man.style.position = 'absolute';
     // var xc = x*50;
@@ -71,13 +71,61 @@ function game(map,b,man,size,count,x,y){
                 count++;
             }
             child.style.border = '1px solid rgb(15, 3, 3)';
-            document.body.append(child);
+            myDiv.append(child);
             if(x == i && y == index){
                 child.appendChild(man);
             }
         }
     }
     return zu;
+}
+
+function update(map,b,man,exist,size,count,x,y,score){
+    let myDiv = document.getElementById('all');
+    let legth = size*50;
+    let attri = legth +'px';
+    myDiv.style.height = '0px';
+    myDiv.style.width = '0px';
+    var count = 0;
+    for (let index = 0; index < size; index++) {
+        for(let i = 0;i < size;++i){
+            const child = document.createElement('div');
+            child.style.height = '50px';
+            child.style.width = '50px';
+            let compu = i*50;
+            let com = index*50;
+            let lef = compu + 'px';
+            let hig = com + 'px';
+            console.log(lef);
+            console.log(hig);
+            child.style.position = 'absolute';
+            child.style.left = lef;
+            child.style.top = hig;
+            var cal = 'child' + i;
+            cal = cal + index;
+            child.id = cal;
+            if(exist[count] == true){
+                count++;
+            }
+            if(map[index][i] == 1){
+            
+                b[count].style.top = hig;
+                b[count].style.left = lef;
+                //document.body.append(child);
+                child.appendChild(b[count]);
+                count++;
+                
+            }
+            child.style.border = '1px solid rgb(15, 3, 3)';
+            myDiv.append(child);
+            if(x == i && y == index){
+                child.appendChild(man);
+            }
+        }
+    }
+    let gameScore = document.querySelector('h2');
+    let onscore = 'score:' + score;
+    gameScore.textContent = onscore;
 }
 
  function delet(map){
